0952b02ee8cbedb358f9e951d4f80e8acdbb9c89
iliyan <iliyan.tsachev@ilnweb.com>
2020-04-29 15:03:40 +0200

    Updates

---
+++ precache-manifest.15cb4ab5d7db8f791cf02113655a60ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f2d5cb63ba1bec01986447ad6165af",
+    "url": "/notifications-component/index.html"
+  },
+  {
+    "revision": "ef0189ec06e36a3eadfa",
+    "url": "/notifications-component/static/css/main.4b635623.chunk.css"
+  },
+  {
+    "revision": "b803fd5a53733df48984",
+    "url": "/notifications-component/static/js/2.6615bc27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/notifications-component/static/js/2.6615bc27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef0189ec06e36a3eadfa",
+    "url": "/notifications-component/static/js/main.b807101b.chunk.js"
+  },
+  {
+    "revision": "1011cb2077b524a7c00b",
+    "url": "/notifications-component/static/js/runtime-main.9ffa8527.js"
+  }
+]);
